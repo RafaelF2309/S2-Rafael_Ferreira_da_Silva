4201e703bc2931bd6bf7420145d3b0437c8ecb07
rafael.ferreira <rafael.f.silva55@aluno.senai.br>
2025-04-07 14:10:09 -0300

    Criação da branch: incluir1
    Código completo com adição da função do calculo à função

--- codigo.py
+++ codigo.py
@@ -3,7 +3,17 @@ def calcular_imc():
     altura = input("Informe sua altura (m): ")
     
     if peso and altura:
-        print("Dados recebidos.")
+        peso = float(peso)
+        altura = float(altura)
+        imc = peso / (altura * altura)
+        print("Seu IMC é:", imc)
+        
+        if imc < 18.5:
+            print("Abaixo do peso.")
+        elif imc < 25:
+            print("Peso ideal.")
+        else:
+            print("Acima do peso.")
     else:
         print("Você precisa informar peso e altura.")
 
